855457f17239c5704ecb3a02c4af4912da5025c5
Semyon Krivtsov <krivtsovs@yahoo.com>
2024-03-21 21:14:47 +0300

    Done task for sprint 5

---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Семён Кривцов on 17.03.2024.
+//
+
+import Foundation
+
+protocol QuestionFactoryDelegate: AnyObject {
+    func didReceiveNextQuestion(question: QuizQuestion?)
+}
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Семён Кривцов on 16.03.2024.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    
+    func requestNextQuestion()
+}
